94c87f028ced089b0f5ac968432ab490b6b16238
wang <583572604@qq.com>
2023-05-09 17:19:15 +0800

    tj

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,55 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+const vscode = require("vscode");
+const fs = require("fs");
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+function activate(context: any) {
+  console.log("插件已经被激活");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	// console.log('Congratulations, your extension "helloWorld" is now active!');
-	console.log('扩展已被激活');
+  // 注册命令
+  let commandOfGetFileState = vscode.commands.registerCommand(
+    "getFileState",
+    (uri: any) => {
+      // 文件路径
+      const filePath = uri.path.substring(1);
+      fs.stat(filePath, (err: any, stats: any) => {
+        if (err) {
+          vscode.window.showErrorMessage(`获取文件时遇到错误了${err}!!!`);
+        }
 
-	const componentsId = 'vscode-extension.components-module';
+        if (stats.isDirectory()) {
+          vscode.window.showWarningMessage(
+            `检测的是文件夹，不是文件，请重新选择！！！`
+          );
+        }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand(componentsId, () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from helloWorld!');
-	});
+        if (stats.isFile()) {
+          const size = stats.size;
+          const createTime = stats.birthtime.toLocaleString();
+          const modifyTime = stats.mtime.toLocaleString();
 
-	context.subscriptions.push(disposable);
+          vscode.window.showInformationMessage(
+            `
+					文件大小为:${size}字节;
+					文件创建时间为:${createTime};
+					文件修改时间为:${modifyTime}
+				`,
+            { modal: true }
+          );
+        }
+      });
+
+      const stats = fs.statSync(filePath);
+      console.log("stats", stats);
+      console.log("isFile", stats.isFile());
+    }
+  );
+
+  // 将命令放入其上下文对象中，使其生效
+  context.subscriptions.push(commandOfGetFileState);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
